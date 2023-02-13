5e327ee7fa8348c8d3f4bb05a88d31f4b4977a1b
Gilbert Veitia Urbay <61420914+gveitia13@users.noreply.github.com>
2023-02-13 00:52:31 -0500

    cap 16

--- inmuebles/settings.py
+++ inmuebles/settings.py
@@ -150,6 +150,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # if not DEBUG:  # Tell Django to copy statics to the `staticfiles` directory
 #     # in your application directory on Render.
 #     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
